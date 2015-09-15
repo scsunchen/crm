047bc03de068a7ec6dc829f4bd9ccde2dc553d80
Nikola Bresliev <nikola.bresliev@gmail.com>
2015-09-15 10:42:24 +0200

    Invoincing start

---
+++ customer-relationship-management/src/main/java/com/invado/customer/relationship/service/dto/InvoicingTransactionSetDTO.java
@@ -0,0 +1,144 @@
+package com.invado.customer.relationship.service.dto;
+
+import com.invado.core.annotations.NativeQueryResultColumn;
+import com.invado.core.annotations.NativeQueryResultEntity;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Nikola on 06/09/2015.
+ */
+@NativeQueryResultEntity
+public class InvoicingTransactionSetDTO {
+
+    @NativeQueryResultColumn(index=0)
+    private Integer distributorId;
+    @NativeQueryResultColumn(index=1)
+    private String distributorName;
+    @NativeQueryResultColumn(index=2)
+    private Integer merchantId;
+    @NativeQueryResultColumn(index=3)
+    private String merchantName;
+    @NativeQueryResultColumn(index=4)
+    private Integer posId;
+    @NativeQueryResultColumn(index=5)
+    private String posName;
+    @NativeQueryResultColumn(index=6)
+    private Integer treminalId;
+    @NativeQueryResultColumn(index=7)
+    private String treminalName;
+    @NativeQueryResultColumn(index=8)
+    private Integer serviceId;
+    @NativeQueryResultColumn(index=9)
+    private String serviceDescription;
+    @NumberFormat(style = NumberFormat.Style.CURRENCY)
+    @NativeQueryResultColumn(index=10)
+    private BigDecimal amount;
+    @NativeQueryResultColumn(index=11)
+    private Integer rownum;
+
+    private Integer i;
+
+
+
+    public Integer getDistributorId() {
+        return distributorId;
+    }
+
+    public void setDistributorId(Integer distributorId) {
+        this.distributorId = distributorId;
+    }
+
+    public String getDistributorName() {
+        return distributorName;
+    }
+
+    public void setDistributorName(String distributorName) {
+        this.distributorName = distributorName;
+    }
+
+    public Integer getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(Integer merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public String getMerchantName() {
+        return merchantName;
+    }
+
+    public void setMerchantName(String merchantName) {
+        this.merchantName = merchantName;
+    }
+
+    public Integer getPosId() {
+        return posId;
+    }
+
+    public void setPosId(Integer posId) {
+        this.posId = posId;
+    }
+
+    public String getPosName() {
+        return posName;
+    }
+
+    public void setPosName(String posName) {
+        this.posName = posName;
+    }
+
+    public Integer getTreminalId() {
+        return treminalId;
+    }
+
+    public void setTreminalId(Integer treminalId) {
+        this.treminalId = treminalId;
+    }
+
+    public String getTreminalName() {
+        return treminalName;
+    }
+
+    public void setTreminalName(String treminalName) {
+        this.treminalName = treminalName;
+    }
+
+    public Integer getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Integer serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public void setServiceDescription(String serviceDescription) {
+        this.serviceDescription = serviceDescription;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Integer getRownum() {
+        return rownum;
+    }
+
+    public void setRownum(Integer rownum) {
+        this.rownum = rownum;
+    }
+
+    public String toString(){
+        return getDistributorId()+" / "+getDistributorName()+" / "+getMerchantId()+" / "+getMerchantName()+" / "+getTreminalName();
+    }
+}
