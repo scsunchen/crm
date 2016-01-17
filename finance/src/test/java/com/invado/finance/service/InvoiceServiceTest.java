9cb653d12cea760de7d0d458dc2e2601f77dcd2c
Dragan Bobic <dragan.bobic@gmail.com>
2016-01-17 18:49:10 +0100

    Add read invoice report unit test.

--- finance/src/test/java/com/invado/finance/service/InvoiceServiceTest.java
+++ finance/src/test/java/com/invado/finance/service/InvoiceServiceTest.java
@@ -22,6 +22,7 @@ import com.invado.core.domain.Properties;
 import com.invado.core.domain.VatPercent;
 import com.invado.core.dto.InvoiceDTO;
 import com.invado.core.dto.InvoiceItemDTO;
+import com.invado.finance.service.dto.InvoiceReportDTO;
 import java.math.BigDecimal;
 import java.security.Permissions;
 import java.time.LocalDate;
@@ -248,5 +249,120 @@ public class InvoiceServiceTest {
         BigDecimal total = (net.multiply(BigDecimal.ONE.add(new BigDecimal(prop.getValue()))))
                 .multiply(dto.getQuantity());
         Assert.assertEquals(0, item1.getTotalCost().compareTo(total));
+    }
+    
+    @Test
+    public void testReadReport() throws Exception {
+        Integer clientId = 1;
+        Integer orgUnitId = 3;
+        String document = "12348/4";
+        Properties generalRate= new Properties("vat_general_rate");
+        generalRate.setValue("0.18");
+        Properties lowRate= new Properties("vat_general_rate");
+        lowRate.setValue("0.08");
+        OrgUnit orgUnit = new OrgUnit(orgUnitId);
+        orgUnit.setClient(new Client(clientId));
+        Invoice invoice = new Invoice(orgUnit, document);
+        invoice.setCreditRelationDate(LocalDate.of(2011, Month.JANUARY, 1));
+        invoice.setInvoiceDate(LocalDate.of(2011, Month.JANUARY, 1));
+        invoice.setValueDate(LocalDate.of(2011, Month.JANUARY, 1));
+        invoice.setPaid(Boolean.FALSE);
+        invoice.setPartner(new BusinessPartner("1009202"));
+        invoice.setInvoiceType(InvoiceType.INVOICE);
+        invoice.setPartnerType(InvoiceBusinessPartner.DOMESTIC);
+        invoice.setPrinted(Boolean.FALSE);
+        invoice.setRecorded(Boolean.FALSE);
+        invoice.setUser(new ApplicationUser("bdragan", new char[]{'p','a','s','s'}));
+        invoice.setVersion(1L);
+        invoice.setContractNumber("032-05/12");
+        invoice.setContractDate(LocalDate.of(2010, Month.JANUARY, 1));
+        Currency currency = new Currency("RSD");
+        invoice.setCurrency(currency);
+        BankCreditor bank = new BankCreditor(1);
+        bank.setName("EFG");
+        bank.setAccount("1234567");
+        invoice.setBank(bank);
+        Article article = new Article("1");
+        article.setDescription("Article");
+        article.setSalePriceWithVAT(BigDecimal.ONE);
+        article.setUnitOfMeasureCode("KOM");
+        article.setUpdated(LocalDate.of(2014, Month.JANUARY, 1));
+        article.setLastUpdateBy(new ApplicationUser(1));
+        article.setVATRate(VatPercent.GENERAL_RATE);
+        article.setVersion(1L);
+
+        InvoiceItem item1 = new InvoiceItem(invoice, 1);
+        item1.setNetPrice(BigDecimal.valueOf(100));
+        item1.setQuantity(BigDecimal.ONE);
+        item1.setArticle(article);
+        item1.setItemDescription(article.getDescription());
+        item1.setUnitOfMeasure(article.getUnitOfMeasureCode());
+        item1.setRabatPercent(BigDecimal.ZERO);
+        item1.setArticleVAT(VatPercent.GENERAL_RATE);
+        item1.setVatPercent(BigDecimal.valueOf(0.18));
+        item1.setTotalCost(BigDecimal.valueOf(118));
+        invoice.addItem(item1);
+
+        InvoiceItem item2 = new InvoiceItem(invoice, 2);
+        item2.setNetPrice(BigDecimal.valueOf(40));
+        item2.setQuantity(BigDecimal.TEN);
+         item2.setArticle(article);
+        item2.setItemDescription(article.getDescription());
+        item2.setUnitOfMeasure(article.getUnitOfMeasureCode());
+        item2.setRabatPercent(BigDecimal.valueOf(0.05));
+        item2.setArticleVAT(VatPercent.GENERAL_RATE);
+        item2.setVatPercent(BigDecimal.valueOf(0.18));
+        item2.setTotalCost(BigDecimal.valueOf(448.4));
+        invoice.addItem(item2);
+
+        expect(EM.find(Invoice.class, new InvoicePK(clientId, orgUnitId, document)))
+                .andReturn(invoice);
+        expect(EM.find(Client.class, invoice.getClientId())).andReturn(new Client(1));
+        expect(EM.find(Properties.class, "vat_general_rate")).andReturn(generalRate);
+        expect(EM.find(Properties.class, "vat_low_rate")).andReturn(lowRate);
+        
+        mocks.replay();
+        InvoiceReportDTO actual = service.readInvoiceReport(
+                clientId, 
+                orgUnitId, 
+                document);
+        mocks.verify();
+
+        Assert.assertEquals(invoice.getDocument(),actual.document);
+        Assert.assertEquals(invoice.getInvoiceDate(), actual.invoiceDate);
+        Assert.assertEquals(invoice.getCreditRelationDate(),actual.creditRelationDate);
+        Assert.assertEquals(invoice.getValueDate(), actual.valueDate);
+        Assert.assertEquals(Boolean.FALSE, actual.printed);
+        Assert.assertEquals(Boolean.TRUE, invoice.isPrinted());
+        Assert.assertEquals( invoice.getVersion(), actual.version);
+        Assert.assertEquals( invoice.getCurrencyISOCode(), actual.currencyISOCode);
+        Assert.assertEquals( invoice.getCurrencyDescription(), actual.currencyDesc);
+        Assert.assertEquals( invoice.getContractNumber(), actual.contractNumber);
+        Assert.assertEquals( invoice.getContractDate(), actual.contractDate);
+        Assert.assertEquals( invoice.getBankName(), actual.bankName);
+        Assert.assertEquals( invoice.getBankAccountNumber(), actual.bankAccount);
+
+        Assert.assertEquals(2, actual.items.size());
+        InvoiceReportDTO.Item actualItem1 = actual.items.get(0);
+        Assert.assertEquals(item1.getVatPercent(), actualItem1.VATPercent);
+        Assert.assertEquals(item1.getOrdinal(), actualItem1.ordinal);
+        Assert.assertEquals(item1.getQuantity(), actualItem1.quantity);
+        Assert.assertEquals(item1.getNetPrice(), actualItem1.netPrice);
+        Assert.assertEquals(item1.getItemDescription(), actualItem1.serviceDesc);
+        Assert.assertEquals(item1.getTotalCost(), actualItem1.itemTotal);
+        Assert.assertEquals(item1.getRebatePercent(), actualItem1.rebatePercent);
+        InvoiceReportDTO.Item actualItem2 = actual.items.get(1);
+        Assert.assertEquals(item2.getVatPercent(), actualItem2.VATPercent);
+        Assert.assertEquals(item2.getOrdinal(), actualItem2.ordinal);
+        Assert.assertEquals(item2.getQuantity(), actualItem2.quantity);
+        Assert.assertEquals(item2.getNetPrice(), actualItem2.netPrice);
+        Assert.assertEquals(item2.getItemDescription(), actualItem2.serviceDesc);
+        Assert.assertEquals(item2.getTotalCost(), actualItem2.itemTotal);
+        Assert.assertEquals(item2.getRebatePercent(), actualItem2.rebatePercent);
+        Assert.assertEquals(0, BigDecimal.valueOf(20).compareTo(actual.rebateTotal));
+        Assert.assertEquals(0, BigDecimal.valueOf(500).compareTo(actual.netPriceTotal));
+        Assert.assertEquals(0, BigDecimal.valueOf(566.4).compareTo(actual.invoiceTotalAmount));
+        Assert.assertEquals(0, BigDecimal.valueOf(480).compareTo(actual.generalRateBasis));
+        Assert.assertEquals(0, BigDecimal.valueOf(86.4).compareTo(actual.generalRateTax));
     }
 }
